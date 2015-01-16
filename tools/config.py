f9e4305a098fdd568e44c64371618602e718a277
Ethan Furman <ethan@stoneleaf.us>
2015-01-16 11:44:24 -0800

    add logconsole to config file blacklist

--- tools/config.py
+++ tools/config.py
@@ -87,7 +87,7 @@ class configmanager(object):
         # Not exposed in the configuration file.
         self.blacklist_for_save = set(
             ['publisher_warranty_url', 'load_language', 'root_path',
-            'init', 'save', 'config', 'update', 'stop_after_init'])
+            'init', 'save', 'config', 'update', 'stop_after_init', 'logconsole'])
 
         # dictionary mapping option destination (keys in self.options) to MyOptions.
         self.casts = {}
