af6de6a07a1a58e972989f6b2f5871383282f378
Ethan Furman <ethan@stoneleaf.us>
2016-06-29 08:05:08 -0700

    correct variable name

--- addons/mail/mail_thread.py
+++ addons/mail/mail_thread.py
@@ -379,7 +379,7 @@ class mail_thread(osv.AbstractModel):
         if context is None:
             context = {}
         force_body = context.get('message_force', '')
-        if not tracked_fields and not message_force:
+        if not tracked_fields and not force_body:
             return True
 
         if isinstance(ids, (int, long)):
