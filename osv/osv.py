3a684f3c543a5225185f79897dbc437e6c1b9d12
Ethan Furman <ethan@stoneleaf.us>
2017-05-04 22:43:46 -0700

    log params during exception

--- osv/osv.py
+++ osv/osv.py
@@ -134,8 +134,10 @@ class object_proxy(object):
                     _logger.info("%s, retry %d/%d in %.04f sec..." % (errorcodes.lookup(e.pgcode), tries, MAX_TRIES_ON_CONCURRENCY_FAILURE, wait_time))
                     time.sleep(wait_time)
                 except orm.except_orm, inst:
+                    _logger.exception('Permissions Error')
                     raise except_osv(inst.name, inst.value)
                 except except_osv:
+                    _logger.exception('Unhandled Exception')
                     raise
                 except IntegrityError, inst:
                     osv_pool = pooler.get_pool(dbname)
