de922af316acc93d5b1126b77807e638b92f621a
Ethan Furman <ethan@stoneleaf.us>
2021-06-15 16:05:35 -0700

    add HR employee-level tests

--- addons/hr/hr.py
+++ addons/hr/hr.py
@@ -99,6 +99,9 @@ class hr_employee_issue(osv.osv):
 
 
 class hr_job(osv.osv):
+    _name = "hr.job"
+    _description = "Job Description"
+    _inherit = ['mail.thread']
 
     def _no_of_employee(self, cr, uid, ids, name, args, context=None):
         res = {}
@@ -117,9 +120,6 @@ class hr_job(osv.osv):
                 res.append(employee.job_id.id)
         return res
 
-    _name = "hr.job"
-    _description = "Job Description"
-    _inherit = ['mail.thread']
     _columns = {
         'name': fields.char('Job Name', size=128, required=True, select=True),
         'expected_employees': fields.function(_no_of_employee, string='Total Forecasted Employees',
@@ -272,6 +272,7 @@ class hr_employee(osv.osv):
                 },
             ),
         'employment_agency_id': fields.many2one('res.partner', 'Employment Agency'),
+        'tests_checks_ids': fields.one2many('hr.test', 'employee_id', 'Tests & Checks'),
         }
 
     fields.apply_groups(
@@ -422,6 +423,100 @@ class hr_department(osv.osv):
         default = default.copy()
         default['member_ids'] = []
         return super(hr_department, self).copy(cr, uid, ids, default, context=context)
+
+
+class hr_test_type(osv.Model):
+    _name = 'hr.test.type'
+    _description = 'test and check types run on employees'
+    _columns = {
+        'name': fields.char('Name', size=32),
+        'description': fields.text('Description'),
+        'result_type': fields.selection(ResultType, 'Result Type'),
+        }
+
+
+class hr_test(osv.Model):
+    _name = 'hr.test'
+    _description = 'checks and tests for (potential) employees'
+    _columns = {
+        'test_type': fields.many2one('hr.test.type', string='Type', ondelete='restrict', required=True),
+        'result_type': fields.related('test_type', 'result_type', string='Result Type', type='selection', selection=ResultType),
+        'result_description': fields.related('test_type', 'description', string='Description', type='text'),
+        'test_date': fields.date('Date'),
+        'result_pass_fail': fields.selection(ResultPassFail, 'Pass/Fail'),
+        'result_grade': fields.selection(ResultGrade, string='Grade'),
+        'result_percent': fields.integer("%"),
+        'result': fields.char('Result', size=4),
+        'employee_id': fields.many2one('hr.employee', 'Employee', ondelete='cascade'),
+        'notes': fields.text('Notes'),
+        'create_date': fields.date('Created on'),
+        }
+
+    def onchange_test(self, cr, uid, ids, test_type, context=None):
+        records = self.pool.get('hr.test.type').read(cr, uid, [('id','=',test_type)], fields=['result_type','description'])
+        result_type = records[0]['result_type']
+        description = records[0]['description']
+        return {
+                'value': {
+                    'result_type': result_type,
+                    'result_description': description,
+                    }}
+
+    def onchange_result(self, cr, uid, ids, result_type, result, context=None):
+        if result_type == 'pass_fail':
+            result = result and ResultPassFail(result).user
+        elif result_type == 'grade':
+            result = result and ResultGrade(result).user
+        elif result_type == 'percent':
+            result = result and str(result)
+        return {
+                'value': {
+                    'result': result,
+                    }}
+
+    def create(self, cr, uid, values, context=None):
+        result_type = values['result_type']
+        if result_type == 'pass_fail':
+            values['result_grade'] = False
+            values['result_percent'] = False
+            result = values.get('result_pass_fail', False)
+            result = result and ResultPassFail(result).user
+        elif result_type == 'grade':
+            values['result_percent'] = False
+            values['result_pass_fail'] = False
+            result = values.get('result_grade', False)
+            result = result and ResultGrade(result).user
+        elif result_type == 'percent':
+            values['result_pass_fail'] = False
+            values['result_grade'] = False
+            result = values.get('result_percent', False)
+            result = result and str(result)
+        values['result'] = result
+        return super(hr_test, self).create(cr, uid, values, context=context)
+
+    def write(self, cr, uid, ids, values, context=None):
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        result_type = values.get('result_type')
+        if result_type is None:
+            result_type = self.read(cr, uid, ids[0], fields=['result_type'], context=context)['result_type']
+        if result_type == 'pass_fail':
+            values['result_grade'] = False
+            values['result_percent'] = False
+            result = values.get('result_pass_fail', False)
+            result = result and ResultPassFail(result).user
+        elif result_type == 'grade':
+            values['result_percent'] = False
+            values['result_pass_fail'] = False
+            result = values.get('result_grade', False)
+            result = result and ResultGrade(result).user
+        elif result_type == 'percent':
+            values['result_pass_fail'] = False
+            values['result_grade'] = False
+            result = values.get('result_percent', False)
+            result = result and str(result)
+        values['result'] = result
+        return super(hr_test, self).write(cr, uid, ids, values, context=context)
 
 class res_partner(osv.Model):
     _name = 'res.partner'
