7fe5e65ba5c382321c2329fd2ed12090bfbcdc16
Ethan Furman <ethan@stoneleaf.us>
2022-04-26 21:51:52 -0700

    fix date handling; add process command

--- scripts/pulse.py
+++ scripts/pulse.py
@@ -10,8 +10,10 @@ from SocketServer import ThreadingMixIn
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from traceback import format_exception
 from urlparse import urlparse, parse_qs
+from datetime import datetime
 import io
 import os
+import re
 import shutil
 import sys
 import threading
@@ -19,8 +21,10 @@ import time
 
 from scription import *
 
-__version__ = "0.1"
+__version__ = "0.11"
 
+
+# server
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 
@@ -32,9 +36,9 @@ class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 
     def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True, log_file=None, msg_location=None):
         self.msg_location = msg_location or os.getcwd()
-        self.today = time.localtime(time.time())[:3]
         self.log_name = log_file
         self.prep_log_file()
+        self.today = time.localtime(time.time())[:3]
         HTTPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate)
 
     def prep_log_file(self):
@@ -44,8 +48,8 @@ class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
         if not os.path.exists(dirs):
             os.path.mkdirs(dirs)
         if not os.path.exists(self.log_name):
-            self.log_file = io.open(self.log_name, 'w', encoding='utf-8')
             self.is_empty_log = True
+        self.log_file = io.open(self.log_name, 'a', encoding='utf-8')
 
     def emit_message(self, msg, timestamp):
         with self.log_lock:
@@ -55,12 +59,16 @@ class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
                 stderr.write(msg)
             else:
                 # time to rotate the file?
-                if not self.is_empty_log and self.today != time.localtime(timestamp)[:3]:
-                    new_name = '%s.%s%s%s' % ((self.log_name, ) + self.today)
+                now = time.localtime(timestamp)[:3]
+                if not self.is_empty_log and self.today != now:
                     self.log_file.close()
-                    shutil.move(self.log_name, new_name)
-                    self.log_file = io.open(self.log_name, 'w', encoding='utf-8')
+                    new_name = '%s.%04d%02d%02d' % ((self.log_name, ) + self.today)
+                    dirs, filename = os.path.split(self.log_name)
+                    shutil.move(self.log_name, os.path.join(dirs, new_name))
+                    self.log_file = io.open(self.log_name, 'a', encoding='utf-8')
+                    self.today = now
                 self.log_file.write(msg)
+                self.log_file.flush()
 
     def handle_error(self, request, client_address):
         cls, exc, tb = sys.exc_info()
@@ -103,21 +111,24 @@ class PulseHTTPRequestHandler(BaseHTTPRequestHandler):
         if query is None:
             query = parse_qs(data.query)
         _, ip, freq, name = data.path.split(u'/', 3)
-        msg_file_name = u'IP-%s-%s-%02d%02d-%05d.txt' % ((ip, name) + self.server.today[1:] + (next(counter), ))
+        timestamp = time.localtime(time.time())
+        msg_file_name = self.server.msg_location + '/' + 'IP-%s-%s-%04d%02d%02d_%02d%02d%02d' % ((ip, name, ) + timestamp)
         # main portion of data for message file
         msg_file_data = [
             "{",
             "'ip_address': %r," % ip,
             "'job_name': %r," % name,
             "'frequency': %r," % freq,
+            "'timestamp': %r," % timestamp,
             ]
         # possible query parameters
         for key, value in query.items():
             msg_file_data.append("'%s': %r," % (key, value[0]))
         msg_file_data.append("}\n")
         msg_file_data = '\n'.join(msg_file_data).decode('utf-8')
-        with io.open(self.server.msg_location+'/'+msg_file_name, 'w', encoding='utf-8') as f:
+        with io.open(msg_file_name+'.tmp', 'w', encoding='utf-8') as f:
             f.write(msg_file_data)
+        shutil.move(msg_file_name+'.tmp', msg_file_name+'.txt')
 
     def log_message(self, format, *args):
         """
@@ -142,7 +153,7 @@ class PulseHTTPRequestHandler(BaseHTTPRequestHandler):
         return s
 
 
-
+# API
 
 @Command(
         port=Spec('port to serve on', type=int),
@@ -153,38 +164,47 @@ def serve(port, log_file, msg_location):
     """
     Run an HTTP server on PORT with logging to LOG-FILE
     """
-    global counter
-    def counter():
-        counter_file = (msg_location or os.getcwd()) + '/counter'
-        if os.path.exists(counter_file):
-            with open(counter_file) as f:
-                i = int(f.read()) + 100
-        else:
-            i = 0
-        lock = threading.Lock()
-        while True:
-            with lock:
-                i += 1
-                i %= 10000
-                if i % 100 == 0:
-                    # write count
-                    with open(counter_file, 'w') as f:
-                        f.write(str(i))
-                yield i 
-    counter = counter()
-
     server_address = ('', port)
-
     httpd = ThreadingHTTPServer(
             server_address,
             PulseHTTPRequestHandler,
             log_file=log_file,
             msg_location=msg_location,
             )
-
     sa = httpd.socket.getsockname()
     echo("Serving HTTP on", sa[0], "port", sa[1], "...")
     httpd.serve_forever()
+
+
+@Command(
+        log_file=Spec('name of file to log to [default: stderr]', ),
+        msg_location=Spec('location to write message files', ),
+        )
+def parse_log(log_file, msg_location):
+    """
+    scan a log file and create message files from its entries (any POST parameters are lost)
+    """
+    match = Var(re.search)
+    with open(log_file) as log:
+        for line in log.readlines():
+            if match(r'\[(\d\d/\w\w\w/\d\d\d\d \d\d:\d\d:\d\d)\] "GET /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\w+)/(.*) HTTP.*"', line):
+                timestamp, ip, freq, name = match().groups()
+                timestamp = datetime.strptime(timestamp, '%d/%b/%Y %H:%M:%S')
+                msg_file_name = timestamp.strftime('IP-%%s-%%s-%Y%m%d_%H%M%S.txt') % (ip, name, )
+                # main portion of data for message file
+                msg_file_data = [
+                    "{",
+                    "'ip_address': %r," % ip,
+                    "'job_name': %r," % name,
+                    "'frequency': %r," % freq,
+                    "'timestamp': %r," % timestamp,
+                    "}\n"
+                    ]
+                msg_file_data = '\n'.join(msg_file_data).decode('utf-8')
+                with io.open(msg_location+'/'+msg_file_name, 'w', encoding='utf-8') as f:
+                    f.write(msg_file_data)
+            else:
+                error('unable to parse line %r' % line)
 
 
 Run()
